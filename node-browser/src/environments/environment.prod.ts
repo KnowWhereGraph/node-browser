392e5d3be8bfe5ab1c4fd95183cf2a7a7f207f42
ThomasThelen <tommythelen@gmail.com>
2024-12-11 10:06:45 -0800

    Add stko build

--- node-browser/src/environments/environment.prod.ts
+++ node-browser/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   baseAddress: 'https://stko-kwg.geog.ucsb.edu/',
-  graphEndpoint: 'https://stko-kwg.geog.ucsb.edu/sparql'
+  graphEndpoint: 'https://stko-kwg.geog.ucsb.edu/sparql',
 };
